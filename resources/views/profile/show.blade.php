a2a20928331b82d4a0c08708bce6cce5a495e36d
Alberto <0056119@academico.ifmg.edu.br>
2024-01-06 13:13:30 -0300

    ajustes tradução

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -5,6 +5,7 @@
         </h2>
     </x-slot>
 
+    @section('content')
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
@@ -42,4 +43,5 @@
             @endif
         </div>
     </div>
+    @endsection
 </x-app-layout>
