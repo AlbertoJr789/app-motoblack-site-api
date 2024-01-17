a1209d23dbb1326032ef2bb1e21e17d71e733785
Alberto <0056119@academico.ifmg.edu.br>
2024-01-16 21:59:10 -0300

    menu sidebar c/ overflow

--- resources/views/components/sidebar-menu.blade.php
+++ resources/views/components/sidebar-menu.blade.php
@@ -1,52 +1,51 @@
-<nav
-    class="group fixed sm:top-0 sm:h-screen sm:flex-col sm:w-20 m-0 w-screen h-20 bottom-0 shadow-slate-300 bg-zinc-900 sm:hover:w-[230px] transition-all duration-300">
-    <ul class="flex sm:flex-col flex-row justify-evenly">
-        <li class="logo mx-auto my-4 px-2 sm:block hidden">
-            <a href="{{ route('dashboard') }}">
-                <x-application-mark />
-            </a>
-        </li>
-        
-        <x-menu-item icon="fa-solid fa-pen">
-            {{ __('Teste') }}
-        </x-menu-item>
-        
-        <x-menu-item icon="fa-solid fa-pen">
-            {{ __('Teste') }}
-        </x-menu-item>
+<div
+    class="group fixed sm:top-0 m-0 bottom-0 sm:w-20  shadow-slate-300 bg-zinc-900 sm:hover:w-[230px] transition-all duration-300">
 
-        <x-menu-item icon="fa-solid fa-pen">
-            {{ __('Teste') }}
-        </x-menu-item>
-        
-        <x-menu-item icon="fa-solid fa-pen">
-            {{ __('Teste') }}
-        </x-menu-item>
+    <div class="bg-zinc-900 mx-auto my-4 px-2 sm:block hidden w-20 sm:group-hover:w-[180px] transition-all duration-300">
+        <a href="{{ route('dashboard') }}">
+            <x-application-mark />
+        </a>
+    </div>
 
+    <div class="bg-zinc-900 flex sm:flex-col flex-row sm:h-full sm:w-auto w-screen sm:hover:overflow-y-scroll overflow-x-auto sm:pb-[10em] sm:pt-5">
         <x-menu-item icon="fa-solid fa-pen">
-            {{ __('Teste') }}
+            {{ __('Teste 1') }}
         </x-menu-item>
-        
         <x-menu-item icon="fa-solid fa-pen">
-            {{ __('Teste') }}
+            {{ __('Teste 2') }}
         </x-menu-item>
-        
         <x-menu-item icon="fa-solid fa-pen">
-            {{ __('Teste') }}
+            {{ __('Teste 3') }}
         </x-menu-item>
-        
         <x-menu-item icon="fa-solid fa-pen">
-            {{ __('Teste') }}
+            {{ __('Teste 4') }}
         </x-menu-item>
-        
         <x-menu-item icon="fa-solid fa-pen">
-            {{ __('Teste') }}
+            {{ __('Teste 5') }}
         </x-menu-item>
-        
         <x-menu-item icon="fa-solid fa-pen">
-            {{ __('Teste') }}
+            {{ __('Teste 6') }}
         </x-menu-item>
-        
-
-    </ul>
-</nav>
+        <x-menu-item icon="fa-solid fa-pen">
+            {{ __('Teste 7') }}
+        </x-menu-item>
+        <x-menu-item icon="fa-solid fa-pen">
+            {{ __('Teste 8') }}
+        </x-menu-item>
+        <x-menu-item icon="fa-solid fa-pen">
+            {{ __('Teste 9') }}
+        </x-menu-item>
+        <x-menu-item icon="fa-solid fa-pen">
+            {{ __('Teste 10') }}
+        </x-menu-item>
+        <x-menu-item icon="fa-solid fa-pen">
+            {{ __('Teste 11') }}
+        </x-menu-item>
+        <x-menu-item icon="fa-solid fa-pen">
+            {{ __('Teste 12') }}
+        </x-menu-item>
+        <x-menu-item icon="fa-solid fa-motorcycle">
+            {{ __('Teste 13') }}
+        </x-menu-item>
+    </div>
+</div>
